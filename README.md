e855bdba34afcfcba2ef9ddc6e9aef865286de6f
mcreach <matthias.creach@gmail.com>
2017-01-24 13:47:16 +0100

    Compatibilité en Python 2.0 Gestion d'erreur dans le cas où le serveur n'est pas lancé.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # client_redis_chat_python
+Le client est compatible avec les version de Python 2.0 et 3.0.
+
+#Prérequis
+Lancer un server Redis
+Host: localhost
+Port: 6379
+
+#Actions
+Pour déconnecter le client du serveur: envoyer le mot "exit" dans le chat
--- test.py
+++ test.py
@@ -1,28 +1,32 @@
 import redis
-
 '''
-Cette méthode est appelée dans un thread et va afficher en boucle les messages du channel qui arrive
-message - le message qui sera affiché 
+Cette methode est appelee dans un thread et va afficher en boucle les messages du channel qui arrive
+message - le message qui sera affiche 
 '''
 
 def my_handler(message):
 	print(message)['data']
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-p = r.pubsub()
-p.subscribe(**{'chat': my_handler})
-thread = p.run_in_thread(sleep_time=0.001)
+try:
+	r = redis.StrictRedis(host='localhost', port=6379, db=0)
+	p = r.pubsub()
+	p.subscribe(**{'chat': my_handler})
+	thread = p.run_in_thread(sleep_time=0.001)
 
-print('Bienvenue')
-pseudo = raw_input('Choisir un pseudo : ')
+	print('Bienvenue')
+	pseudo = raw_input('Choisir un pseudo : ')
 
-msg = None
-while(msg != 'exit'):
-	msg = raw_input()
-	if(msg != 'exit'):
-		r.publish('chat', pseudo + ' : ' + msg)
+	msg = None
+	while(msg != 'exit'):
+		msg = raw_input()
+		if(msg != 'exit'):
+			r.publish('chat', pseudo + ' : ' + msg)
 
-thread.stop()
+	thread.stop()
+except redis.exceptions.ConnectionError as e:
+	print "Erreur - Le serveur Redis n'a pas ete lance !"
+
+
 
 
 	
